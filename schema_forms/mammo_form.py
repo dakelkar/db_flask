2826dbd079c29308418ad12966f083d56355a9e9
zaphod <dakelkar@gmail.com>
2018-04-25 23:00:32 +0530

    removed unnecessary prefixes

--- schema_forms/mammo_form.py
+++ schema_forms/mammo_form.py
@@ -9,25 +9,25 @@ class MammoArchDistortionsForm(FlaskForm):
     class Meta:
         csrf = False
 
-    architectural_distortions_location_right_breast = SelectField("Location of Architectural Distortion on Right Breast",
+    fld_loc_right_breast = SelectField("Location of Architectural Distortion on Right Breast",
                                                                   choices = MammographyDict.
                                                                   mammo_arch_location_right_breast_choice)
-    architectural_distortions_location_left_breast = SelectField("Location of Architectural Distortion on Left Breast",
+    fld_loc_left_breast = SelectField("Location of Architectural Distortion on Left Breast",
                                                                  choices = MammographyDict.
                                                                  mammo_arch_location_left_breast_choice)
-    architectural_distortions_depth =  SelectField("Depth of Architectural Distortion", choices =
+    fld_depth =  SelectField("Depth of Architectural Distortion", choices =
                                         MammographyDict.mammo_arch_depth_choice)
-    architectural_distortions_dist = SelectField("Distance of Architectural Distortion from nipple", choices =
+    fld_dist = SelectField("Distance of Architectural Distortion from nipple", choices =
                                     MammographyDict.mammo_arch_dist_choice)
 
 
     def to_bson(self):
-        bson = form_utilities.to_bson(self, prefix="architectural_distortions_")
+        bson = form_utilities.to_bson(self)
         return bson
 
 
     def from_bson(self, p):
-        form_utilities.from_bson(self, p, prefix="architectural_distortions_")
+        form_utilities.from_bson(self, p)
 
 # to figure out validators...
 
@@ -36,43 +36,43 @@ class MammoMassForm(FlaskForm):
     class Meta:
         csrf = False
 
-    mammo_mass_number = IntegerField("Mass detected", default=1)
-    mammo_mass_location_right_breast = SelectField("Location of mass in Right Breast",
+    fld_number = IntegerField("Mass detected", default=1)
+    fld_loc_right_breast = SelectField("Location of mass in Right Breast",
                                                    choices=MammographyDict.mammo_mass_location_right_breast_choice)
-    mammo_mass_location_left_breast = SelectField("Location of mass in Left Breast",
+    fld_loc_left_breast = SelectField("Location of mass in Left Breast",
                                                   choices=MammographyDict.mammo_mass_location_left_breast_choice)
-    mammo_mass_depth = SelectField("Depth of mass detected", choices=MammographyDict.mammo_mass_depth_choice)
-    mammo_mass_dist = SelectField("Distance of mass from nipple", choices=MammographyDict.mammo_mass_dist_choice)
-    mammo_mass_pect = StringField("Distance from Pectoralis Major (cm) if available (NA if not present)")
-    mammo_mass_shape = SelectField("Shape of mass", choices=MammographyDict.mammo_mass_shape_choice)
-    mammo_mass_margin = SelectField("Margins of mass", choices=MammographyDict.mammo_mass_margin_choice)
-    mammo_mass_density = SelectField("Density of mass", choices=MammographyDict.mammo_mass_density_choice)
+    fld_depth = SelectField("Depth of mass detected", choices=MammographyDict.mammo_mass_depth_choice)
+    fld_dist = SelectField("Distance of mass from nipple", choices=MammographyDict.mammo_mass_dist_choice)
+    fld_pect = StringField("Distance from Pectoralis Major (cm) if available (NA if not present)")
+    fld_shape = SelectField("Shape of mass", choices=MammographyDict.mammo_mass_shape_choice)
+    fld_margin = SelectField("Margins of mass", choices=MammographyDict.mammo_mass_margin_choice)
+    fld_density = SelectField("Density of mass", choices=MammographyDict.mammo_mass_density_choice)
 
 
     def to_bson(self):
-        bson = form_utilities.to_bson(self, prefix="mammo_mass")
+        bson = form_utilities.to_bson(self)
         return bson
 
 
     def from_bson(self, p):
-        form_utilities.from_bson(self, p, prefix="mammo_mass")
+        form_utilities.from_bson(self, p)
 
 class MammoCalcificationForm(FlaskForm):
 
     class Meta:
         csrf = False
 
-    mammo_calcification_number = IntegerField("Group of calcification", default= 1)
-    mammo_calcification_location_right_breast = SelectField("Location of calcification in Right Breast",
+    fld_number = IntegerField("Group of calcification", default= 1)
+    fld_loc_right_breast = SelectField("Location of calcification in Right Breast",
                                                        choices=MammographyDict.mammo_calc_location_right_breast_choice)
-    mammo_calcification_location_left_breast = SelectField("Location of calcification in Left Breast",
+    fld_loc_left_breast = SelectField("Location of calcification in Left Breast",
                                                       choices=MammographyDict.mammo_calc_location_left_breast_choice)
-    mammo_calcification_depth = SelectField("Depth of Calcification", choices=MammographyDict.mammo_calc_depth_choice)
-    mammo_calcification_dist = SelectField("Distance of calcification from nipple",
+    fld_depth = SelectField("Depth of Calcification", choices=MammographyDict.mammo_calc_depth_choice)
+    fld_dist = SelectField("Distance of calcification from nipple",
                                       choices=MammographyDict.mamo_calc_dist_choice)
-    mammo_calcification_type = SelectField("Calcification Type",
+    fld_type = SelectField("Calcification Type",
                                                choices=MammographyDict.mammo_calcification_type_choice)
-    mammo_calcification_diagnosis = SelectField("Calcification Diagnosis",
+    fld_diagnosis = SelectField("Calcification Diagnosis",
                                                     choices=MammographyDict.mammo_calcification_diagnosis_choice)
 
 
@@ -102,9 +102,9 @@ class MammographyForm(FlaskForm):
     fld_mammo_calcification_present = SelectField("Are there any calcifications detected?", choices=
                                                     MammographyDict.mammo_calcification_present_choice)
     mammo_calcification = FormField(MammoCalcificationForm)
-    fld_architectural_distortions_present = SelectField("Are Architectural Distortions present", choices =
+    fld_mammography_architectural_distortions_present = SelectField("Are Architectural Distortions present", choices =
                                         MammographyDict.mammo_arch_present_choice)
-    architectural_distortions = FormField(MammoArchDistortionsForm)
+    mammography_architectural_distortions = FormField(MammoArchDistortionsForm)
     # if set validator here?
     fld_mammo_assym_present = SelectField("Are Asymmetries present", choices =  MammographyDict.mammo_assym_present_choice)
     fld_mammo_assym_location_right_breast = SelectField("Location of asymmetries on Right Breast", choices =
@@ -150,7 +150,7 @@ class MammographyForm(FlaskForm):
         bson['last_update'] = datetime.today()
 
         #spl stuff for subforms
-        bson['architectural_distortions'] = self.architectural_distortions.to_bson()
+        bson['mammography_architectural_distortions'] = self.mammography_architectural_distortions.to_bson()
         bson['mammo_mass'] = self.mammo_mass.to_bson()
         bson['mammo_calcification'] = self.mammo_calcification.to_bson()
         return bson
@@ -161,7 +161,7 @@ class MammographyForm(FlaskForm):
         self.mammo_date.data = p['mammo_date'].date()
         self.last_update.data = p['last_update'].date()
         #spl stuff for subforms
-        self.architectural_distortions.from_bson(p['architectural_distortions'])
+        self.mammography_architectural_distortions.from_bson(p['mammography_architectural_distortions'])
         self.mammo_mass.from_bson(p['mammo_mass'])
         self.mammo_calcification.from_bson(p['mammo_calcification'])
 
@@ -172,5 +172,5 @@ class MammographyForm(FlaskForm):
 #$function: should find "other", if False, then new field samefield_other is hidden, but when true is seen so
 # dont need new subform, registers as on change function
 
-#keep last update, user as hidden...
+#figure out how to get user passed to this form...
 
